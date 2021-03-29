242d1039b52e369c19a2e0ce8c2f63d8a25dafd3
s3719633 <s3719633@student.rmit.edu.au>
2021-03-30 10:19:06 +1100

    [Task] Reverted changes due to errors

--- src/jest.config.js
+++ src/jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
     // collectCoverage: false,
   
     // An array of glob patterns indicating a set of files for which coverage information should be collected
-    collectCoverageFrom: ["src/models/*.js"],
+    collectCoverageFrom: ["models/*.js"],
   
     // The directory where Jest should output its coverage files
     coverageDirectory: "coverage",
